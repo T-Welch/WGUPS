14cfd55c18cfa80d9401c7fe5d7536933440f0ea
Trevor Welch <trevor@goosefam.com>
2023-09-14 19:55:00 -0700

    created menu class, started implementation of menu in main

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import csv
+import random
 from package import Package
 import chainingHashTable
 import distance
+import menu
+from datetime import date, datetime, timedelta
 
 if __name__ == "__main__":    
+    
     def loadPackageDataAndInsertIntoHashTable():
         with open('packages.csv', newline='') as csvfile:
             # PD will be shorthand for package data for ease of use
@@ -29,7 +33,30 @@ if __name__ == "__main__":
                 #except AttributeError:
                 #    print('non issue')
         
-        
+    def userInterface():
+        interface = menu.Menu
+        interface.welcomeMessage()
+        while True:
+            interface.optionMenu()
+            userInput = input('Please pick an option: ')
+            if userInput.lower() in ['q', 'quit', 'exit', 'end', 'stop']:
+                print(interface.MOTD())
+                break
+            if userInput.lower() in ['m', 'menu', 'help','?']:
+                print('\n\n')
+                interface.optionMenu()
+            if userInput.lower()[0] == 's':
+                try:
+                    time = datetime.strptime(userInput[2:], '%H:%M').time()
+                    print(str(type(time)) + str(time))
+                    continue
+                except ValueError:
+                    print('\n**********************************************\n')
+                    print('Invalid time value. Please use HH:MM ex: 09:30')
+                    print('\n**********************************************\n')
+                    continue
+    
+    userInterface()
             
     loadPackageDataAndInsertIntoHashTable()
     
@@ -40,5 +67,5 @@ if __name__ == "__main__":
     # print(dist.returnAddressIndex('HUB'))
     #print(dist.distance_table[11][1])
     #print(dist.returnDistance(11,0))
-    print(dist.returnDistance(dist.returnAddressIndex('HUB'), dist.returnAddressIndex('3365 S 900 W')))
+    #print(dist.returnDistance(dist.returnAddressIndex('HUB'), dist.returnAddressIndex('3365 S 900 W')))
             
---
+++ menu.py
@@ -0,0 +1,43 @@
+import random
+class Menu:
+    
+    def MOTD():
+        motds = [
+        "I hope you have a good day and good things happen to you.",
+        "Remember, every moment is an opportunity to shine.",
+        "May today be filled with sunshine and smiles for you.",
+        "Sending positive vibes your way for a wonderful day ahead!",
+        "Every challenge you face is an opportunity to grow.",
+        "Today is a blank canvas, paint it beautifully.",
+        "May your day overflow with joy and success.",
+        "Wishing you strength and serenity for today.",
+        "Embrace the present moment and find joy in it.",
+        "You're capable of amazing things. Never forget that.",
+        "Today, be the reason someone else smiles.",
+        "Believe in the power of positivity today.",
+        "Sending you energy and light for the day ahead.",
+        "Remember, kindness and patience go a long way.",
+        "Take a deep breath, cherish the moment, and move forward with confidence."
+        ]
+        return(random.choice(motds))
+    
+    #this is unnccessary, just for looks
+    
+    def welcomeMessage():
+        print('**********************************************************************************\n')
+        print('                    Welcome to Trevor\'s WGUPS implementation!')
+        print('          Please refer to this menu in order to interact with this program.')
+        print('              ' + Menu.MOTD())
+        print('\n**********************************************************************************\n\n')
+        
+    #created a function for this for extensibility later for portfolio project
+    
+    def optionMenu():
+        print('Enter (Q) to quit.')
+        print('Enter M to display this menu')
+        print('Enter (S) + [time] to view the status of packages at a given time.')
+        print('An example of a status lookup request is as follows:')
+        print('S 9:30')
+        
+        
+        
